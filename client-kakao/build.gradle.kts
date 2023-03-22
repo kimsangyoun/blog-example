520a3156a648e572f5b25c726047d1e438f013a9
kimsangyoun <ksang91@naver.com>
2023-03-23 00:25:31 +0900

    feat : bootjar 실행 추가..

--- client-kakao/build.gradle.kts
+++ client-kakao/build.gradle.kts
@@ -1,3 +1,4 @@
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-webflux")
+    implementation("io.netty:netty-resolver-dns-native-macos:4.1.79.Final:osx-aarch_64")
 }
